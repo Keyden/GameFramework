03c635210d936dde173b3a3354888545235852ee
keyden <shenchengye123@126.com>
2020-12-04 16:15:07 +0800

    最大堆

--- Client/Assets/Scripts/Core/Collections/Heap/MaxHeap.cs
+++ Client/Assets/Scripts/Core/Collections/Heap/MaxHeap.cs
@@ -2,17 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MaxHeap : MonoBehaviour
+namespace GameFramwork.Collections
 {
-    // Start is called before the first frame update
-    void Start()
+    public class MaxHeap 
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
